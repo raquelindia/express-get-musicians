3dd6fa35b50ba9f4385f5779cb9531fa8a86c321
raquelindia <raquelicruz94@gmail.com>
2023-03-06 15:51:08 -0500

    add express route

--- server.js
+++ server.js
@@ -3,11 +3,16 @@ const app = express();
 const {Musician} = require("./Musician")
 const {sequelize} = require("./db")
 
-const port = 3000;
+const port = 4000;
 
 //TODO
 
+app.get("/musicians", async (request, response) => {
+    const getMusician = await Musician.findAll();
+    response.json(getMusician);
+})
+
 app.listen(port, () => {
     sequelize.sync();
-    console.log(`Listening on port ${port}`)
+    console.log(`Listening on port http://localhost:${port}/musicians`);
 })
